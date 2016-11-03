4df139601f7f5d1973c7c4cd345e427d6aab6d4c
hernanblanco073 <hernanblanco.073@gmail.com>
2016-11-03 13:41:10 -0300

    se agrega grilla (lista) de accidentes

--- Autopista/www/js/grillaCtrl.js
+++ Autopista/www/js/grillaCtrl.js
@@ -1,7 +1,17 @@
 angular.module('starter.controladorGrilla', [])
 
-.controller('grillaCtrl', function($scope) {
+.controller('grillaCtrl', function($scope, $firebaseArray, $timeout, $state) {
 
-  
+	var FBRef = new Firebase("https://myapp-d5d9c.firebaseio.com/Accidentes");
+ 	$timeout($scope.ListadoAccidentes = $firebaseArray(FBRef));
+
+ 	console.info($scope.ListadoAccidentes);
+
+ 	$scope.VerMapa = function(lat,long){
+
+ 		alert(lat + long);
+
+ 	};
+
 })
 
