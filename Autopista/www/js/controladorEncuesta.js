9de4949354382fd75ff30bc532e211e0f00a210b
pabloigoldi <pablo.emanuel.ig@gmail.com>
2016-11-12 11:14:38 -0300

    Cambios encuesta.

--- Autopista/www/js/controladorEncuesta.js
+++ Autopista/www/js/controladorEncuesta.js
@@ -2,14 +2,15 @@ angular.module('starter.controladorEncuesta', [])
 
 .controller('EncuestaCtrl', function($scope, $state, $cordovaGeolocation, $firebaseArray,Data,Servicio, $timeout) {
     
-	$scope.encuesta = {};
-	
+  $scope.encuesta = {};
+  
     $scope.encuesta.respuestas = {};    
     $scope.encuesta.respuestas.dondeConocio= "";
     $scope.encuesta.respuestas.usoHabitual = "";
     $scope.encuesta.respuestas.queImpresiono = "";
     $scope.encuesta.respuestas.queDecepciono = "";
     $scope.encuesta.respuestas.loRecomendaria = "";
+    $scope.encuesta.respuestas.sugerencia = "";
 
     $scope.encuesta.fecha = Date();
     $scope.encuesta.usuario = {};
@@ -19,22 +20,22 @@ angular.module('starter.controladorEncuesta', [])
     referencia.on('value', function(snapshot) {
     $timeout(function() {
           $scope.encuesta.usuario = snapshot.val();    
-       	});
+        });
     });    
   
     $scope.guardarEncuesta=function(){    
         try{
-          	console.log($scope.encuesta);
-          	Servicio.Guardar("Encuestas",$scope.encuesta);
-      		firebase.auth().signOut().catch(function (error){
-        	console.info("Ha ocurrido un error en la encuesta. " + error);
-      		}).then( function(resultado){
-        		$state.go("app.inicio");
-      		});
-      	}catch(error)
-    	{
-      		console.info("Ha ocurrido un error en la encuesta. " + error);
-    	}
+            console.log($scope.encuesta);
+            Servicio.Guardar("Encuestas/" + $scope.encuesta.usuario.nombre +"/" + String($scope.encuesta.fecha) +"/" ,$scope.encuesta);
+          firebase.auth().signOut().catch(function (error){
+          console.info("Ha ocurrido un error en la encuesta. " + error);
+          }).then( function(resultado){
+            $state.go("app.inicio");
+          });
+        }catch(error)
+      {
+          console.info("Ha ocurrido un error en la encuesta. " + error);
+      }
     }
 
 });
