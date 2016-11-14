467ff99a099b73eb50abaee12bff16a34fe91af0
hernanblanco073 <hernanblanco.073@gmail.com>
2016-11-13 21:14:40 -0300

    Muestra la hora con formato correcto en la grilla, no todo el timestamp entero

--- Autopista/www/js/controladorGrilla.js
+++ Autopista/www/js/controladorGrilla.js
@@ -3,16 +3,35 @@ angular.module('starter.controladorGrilla', [])
 .controller('grillaCtrl', function($scope, $timeout, $state, Servicio, FactoryUsuario) {
 
 	$scope.ListadoAccidentes = [];
+	var i = 0;
 	Servicio.Cargar('/Accidentes')
 		.on('child_added',function(snapshot)
 			{
+				var fecha_parse = new Date(snapshot.val().fecha);
+				var anio = fecha_parse.getFullYear();
+				var mes = fecha_parse.getMonth();
+				var dia = fecha_parse.getDate();
+				var hora = fecha_parse.getHours();
+				var minutos = fecha_parse.getMinutes();
+				if(minutos.toString().length == 1)
+				{
+					minutos = "0" + minutos;
+				}
+				if(hora.toString().length == 1)
+				{
+					hora = "0" + hora;
+				}
+
+				var fecha = dia+"/"+mes+"/"+anio+" "+hora+":"+minutos;
 				$scope.ListadoAccidentes.push(snapshot.val());
+				$scope.ListadoAccidentes[i].fecha = fecha;
+				i = i + 1;
 			});
 
 	//var FBRef = new Firebase("https://myapp-d5d9c.firebaseio.com/Accidentes");
   	//$scope.ListadoAccidentes = $firebaseArray(FBRef);
 
- 	console.info($scope.ListadoAccidentes);
+ 	
 
  	$scope.VerMapa = function(accidente){
 
